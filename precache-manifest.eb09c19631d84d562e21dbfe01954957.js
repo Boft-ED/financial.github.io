553e2b12d0f99ca5030d39009a4e89b8176dd198
Nasekin <nasekinwhiteorblack@gmail.com>
2023-09-25 22:14:18 +0300

    add new changes

---
+++ precache-manifest.eb09c19631d84d562e21dbfe01954957.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5162b6fe395bf44e41e1",
+    "url": "/financial.github.io/static/css/main.2fca9ac3.chunk.css"
+  },
+  {
+    "revision": "5162b6fe395bf44e41e1",
+    "url": "/financial.github.io/static/js/main.5162b6fe.chunk.js"
+  },
+  {
+    "revision": "4587b35573b7dda0a77d",
+    "url": "/financial.github.io/static/js/1.4587b355.chunk.js"
+  },
+  {
+    "revision": "9891c9e89175b8dde865",
+    "url": "/financial.github.io/static/js/2.9891c9e8.chunk.js"
+  },
+  {
+    "revision": "2cf2bf1cc1aba7ced28e",
+    "url": "/financial.github.io/static/js/runtime~main.2cf2bf1c.js"
+  },
+  {
+    "revision": "c103e4e10665c8f3ec068a2d1d675ef3",
+    "url": "/financial.github.io/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4ef3d62dc54e58b46f42f6457dcb369e.js"
+  "/financial.github.io/precache-manifest.eb09c19631d84d562e21dbfe01954957.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/financial.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
